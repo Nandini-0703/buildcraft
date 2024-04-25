a03637fe23f813b972649f3f66065f0459ec2a21
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-04-26 01:26:25 +0530

    fix tailwind text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuroraBackground } from "./components/ui/aurora-background";
 import HomePage from "./components/Home/home";
 
 const App = () => {
